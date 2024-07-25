685246dbf8699297b5ea87fedeb6ac7d69f23b47
Linus Bolls <linusbolls@Linuss-MacBook-Air-3.local>
2024-07-25 22:22:52 +0200

    feat: mock fact page

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "@fastify/cors";
 
 import { routes } from "./routes";
 import { env } from "./env";
+import { dummyFactPage } from "./dummyFactPage";
 
 async function main() {
   const fastify = Fastify({ logger: true });
@@ -14,18 +15,7 @@ async function main() {
   fastify.get("/f/:factId", (request, reply) => {
     const factId = (request.params as any).factId;
 
-    reply.type("text/html").send(`<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-  <h1>${factId}</h1>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/x0uiD362T48?si=tghb2ZEAsOZPyKaw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-  </body>
-</html>`);
+    reply.type("text/html").send(dummyFactPage);
   });
 
   fastify.listen({ port: env.port }, (err, address) => {
